bebf1137b6dbb8b24ee483221ab09a543ef17ac9
DreherFRC2815 <bluemevilmechanics@gmail.com>
2017-01-31 03:54:28 -0500

    Initial Commit

---
+++ src/org/usfirst/frc/team2815/robot/OI.java
@@ -0,0 +1,82 @@
+package org.usfirst.frc.team2815.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	Joystick xbox = new Joystick(0);
+	Joystick flight = new Joystick(1);
+
+	public double getLeftYValue() {
+		if (Math.abs(xbox.getRawAxis(1)) >= .05) {
+			return (xbox.getRawAxis(1));
+		} else
+			return 0;
+	}
+	
+	public double getLeftXValue() {
+		if (Math.abs(xbox.getRawAxis(0)) >= .05) {
+			return (xbox.getRawAxis(0));
+		} else
+			return 0;
+	}
+
+	public double getRightYValue() {
+		if (Math.abs(xbox.getRawAxis(5)) >= .05) {
+			return (-1 * xbox.getRawAxis(5));
+		} else
+			return 0;
+	}
+
+	public double getRightXValue() {
+		if (Math.abs(xbox.getRawAxis(4)) >= .1) {
+			return (xbox.getRawAxis(4));
+		} else
+			return 0;
+	}
+
+	public boolean getAButton() {
+		return xbox.getRawButton(1);
+	}
+
+	public boolean getXButton() {
+		return xbox.getRawButton(3);
+	}
+
+	public double getRightTrigger() {
+		return xbox.getRawAxis(3);
+	}
+
+	public double getLeftTrigger() {
+		return xbox.getRawAxis(2);
+	}
+
+	public int getThePOV() {
+		return xbox.getPOV();
+	}
+
+	public boolean getFlight1() {
+		return flight.getRawButton(1);
+	}
+
+	public boolean getFlight2() {
+		return flight.getRawButton(2);
+	}
+
+	public boolean getFlight3() {
+		return flight.getRawButton(3);
+	}
+
+	public boolean getFlight4() {
+		return flight.getRawButton(4);
+	}
+
+	public double getFlightY() {
+		return flight.getRawAxis(1);
+	}
+}
---
+++ src/org/usfirst/frc/team2815/robot/subsystems/DriveTrain.java
@@ -0,0 +1,90 @@
+package org.usfirst.frc.team2815.robot.subsystems;
+
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	RobotDrive mecanum;
+	
+	ADXRS450_Gyro gyro;
+	
+	double P,I,D,F;
+	int izone;
+	int profile;
+	
+	public CANTalon[] SRXMotors = new CANTalon[4];
+	// 0 1
+	// 2 3
+	public DriveTrain(){
+		gyro = new ADXRS450_Gyro();
+		
+		P = .3;
+		I = .003;
+		D = 3;
+		F = .0003;
+		
+		izone = 7;
+		
+		profile = 0;
+		SRXMotors[0] = new CANTalon(0);
+		SRXMotors[1] = new CANTalon(1);
+		SRXMotors[2] = new CANTalon(2);
+		SRXMotors[3] = new CANTalon(3);
+		
+		SRXMotors[0].changeControlMode(TalonControlMode.Speed);
+		SRXMotors[0].changeControlMode(TalonControlMode.Speed);
+		SRXMotors[0].changeControlMode(TalonControlMode.Speed);
+		SRXMotors[0].changeControlMode(TalonControlMode.Speed);
+		
+		SRXMotors[0].setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		SRXMotors[0].setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		SRXMotors[0].setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		SRXMotors[0].setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		
+		SRXMotors[0].configNominalOutputVoltage(+0f, -0f);
+		SRXMotors[0].configNominalOutputVoltage(+0f, -0f);
+		SRXMotors[0].configNominalOutputVoltage(+0f, -0f);
+		SRXMotors[0].configNominalOutputVoltage(+0f, -0f);
+		
+		SRXMotors[0].configPeakOutputVoltage(+12f, -12f);
+		SRXMotors[0].configPeakOutputVoltage(+12f, -12f);
+		SRXMotors[0].configPeakOutputVoltage(+12f, -12f);
+		SRXMotors[0].configPeakOutputVoltage(+12f, -12f);
+		
+		SRXMotors[0].setCloseLoopRampRate(2);
+		SRXMotors[0].setCloseLoopRampRate(2);
+		SRXMotors[0].setCloseLoopRampRate(2);
+		SRXMotors[0].setCloseLoopRampRate(2);
+		
+		SRXMotors[0].setVoltageRampRate(2);
+		SRXMotors[0].setVoltageRampRate(2);
+		SRXMotors[0].setVoltageRampRate(2);
+		SRXMotors[0].setVoltageRampRate(2);
+		
+		SRXMotors[0].setPID(P, I, D, F, izone, 2, profile);
+		SRXMotors[0].setPID(P, I, D, F, izone, 2, profile);
+		SRXMotors[0].setPID(P, I, D, F, izone, 2, profile);
+		SRXMotors[0].setPID(P, I, D, F, izone, 2, profile);
+	}
+	
+	public void driveMecanumGyro(double y, double x, double zTurn){
+		mecanum.mecanumDrive_Cartesian(x, y, zTurn, gyro.getAngle());
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+}
+
